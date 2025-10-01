91dafedaf00d9d8ba7880441c5df44b3872df269
ArtemiusMiave1 <artemiusmiave@y7mail.com>
2025-10-01 16:24:41 +1000

    imported and testing shaders

--- Assets/Shaders/Outlines.hlsl
+++ Assets/Shaders/Outlines.hlsl
@@ -3,7 +3,7 @@
 #define BACKFACEOUTLINES_INCLUDED
 
 // Include helper functions from URP
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+//#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
 
 // Data from the meshes struct Attributes {
 struct Attributes
@@ -15,15 +15,28 @@ struct Attributes
     
 
 // Output from the vertex function and input to the fragment function struct VertexOutput {
-float4 positionCS
-: SV_POSITION; // Position in clip space
+struct Attributes
+{
+    float4 positionCS : SV_POSITION; // Position in clip space
+};
+    
 // Properties
 float _Thickness;
 float4 _Color;
-VertexOutput Vertex(Attributes input) { VertexOutput output = (VertexOutput)0;
-float3 normalOS = input.normalOS;
-// Extrude the object space position along a normal vector float3 posOS = input.positionOS.xyz + normalOS * _Thickness; // Convert this position to world and clip space
-output.positionCS = GetVertexPositionInputs(posOS).positionCS;
-return output;
-float4 Fragment (VertexOutput input) : SV_Target return _Color;
+
+VertexOutput Vertex(Attributes input)
+{
+    VertexOutput output = (VertexOutput) 0;
+
+    float3 normalOS = input.normalOS;
+
+    // Extrude the object space position along a normal vector 
+    float3 posOS = input.positionOS.xyz + normalOS * _Thickness; 
+    // Convert this position to world and clip space
+    output.positionCS = GetVertexPositionInputs(posOS).positionCS;
+    return output;
+}
+float4 Fragment(VertexOutput input) : SV_Target {
+    return _Color;
+}
 #endif
